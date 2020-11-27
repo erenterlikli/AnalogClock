3a601bb971d29d5fa5c72a28a101765b9e01166d
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-11-27 17:06:34 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AnalogSaat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(666, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(666, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Saat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogSaat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //global değişkenlerimiz.
+        Timer t = new Timer();
+        int Genislik = 300, Yukseklik = 300, Snİbre = 140, Dkİbre = 110, Saatİbre = 80;
+        int OrtaX, OrtaY;
+        Bitmap bmp;
+        Graphics g;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bmp);
+            int ss = DateTime.Now.Second;
+            int mm = DateTime.Now.Minute;
+            int hh = DateTime.Now.Hour;
+
+            int[] ibrekoordinat = new int[2]; //indis 0 için x ekseni, indis 1 için y ekseni.
+            g.Clear(Color.White);
+
+            g.DrawEllipse(new Pen(Color.Black, 1F), 0, 0, Yukseklik, Genislik);
+            g.DrawString("12", new Font("Arial", 12), Brushes.Black, new PointF(140, 2)); //Saat 12 için.
+
+            g.DrawEllipse(new Pen(Color.Black, 1F), 0, 0, Yukseklik, Genislik);
+            g.DrawString("3", new Font("Arial", 12), Brushes.Black, new PointF(286, 140)); //Saat 3 için.
+
+            g.DrawEllipse(new Pen(Color.Black, 1F), 0, 0, Yukseklik, Genislik);
+            g.DrawString("6", new Font("Arial", 12), Brushes.Black, new PointF(142, 282)); //Saat 6 için.
+
+            g.DrawEllipse(new Pen(Color.Black, 1F), 0, 0, Yukseklik, Genislik);
+            g.DrawString("9", new Font("Arial", 12), Brushes.Black, new PointF(0, 140)); //Saat 9 için.
+
+          
+
+            //saniye için çizdirme.
+            ibrekoordinat = mscoord(ss, Snİbre);
+            g.DrawLine(new Pen(Color.Red, 1F), new Point(OrtaX, OrtaY), new Point(ibrekoordinat[0], ibrekoordinat[1]));
+
+            //dakika için yelkovan çizdirme.
+            ibrekoordinat = mscoord(mm, Dkİbre);
+            g.DrawLine(new Pen(Color.Green, 2F), new Point(OrtaX, OrtaY), new Point(ibrekoordinat[0], ibrekoordinat[1]));
+
+            //saat için akrep çizdirme. Saat 12de bir başa döndüğü için modu aldık.
+            ibrekoordinat = hrcoord(hh%12,mm, Saatİbre);
+            g.DrawLine(new Pen(Color.Red, 3F), new Point(OrtaX, OrtaY), new Point(ibrekoordinat[0], ibrekoordinat[1]));
+
+            pictureBox1.Image = bmp;
+            this.Text = "Saat:" + hh + ":" + mm + ":" + ss;
+            g.Dispose();
+        }
+
+        private int[] mscoord(int sndeger, int saataci) ///dakika ve saniye için.
+        {
+            int [] coord = new int [2];
+            sndeger *= 6;
+
+            if(sndeger>=0 &&  sndeger<=180)
+            {
+                coord[0] = OrtaX + (int)(saataci * Math.Sin(Math.PI * sndeger / 180));
+                coord[1] = OrtaY - (int)(saataci * Math.Cos(Math.PI * sndeger / 180));
+            }
+
+            else
+            {
+                coord[0] = OrtaX - (int)(saataci * -Math.Sin(Math.PI * sndeger / 180));
+                coord[1] = OrtaY - (int)(saataci * Math.Cos(Math.PI * sndeger / 180));
+            }
+            return coord;
+
+        }
+
+        int[] coord = new int[2];
+
+        private int[] hrcoord(int saatdeger,int dkdeger,int saataci) //saat için.
+        {
+            int val=(int)((saatdeger*30)+dkdeger*0.5);
+
+            if(val>=0 && val<=180)
+            {
+                coord[0] = OrtaX + (int)(saataci * Math.Sin(Math.PI * val / 180));
+                coord[1] = OrtaY - (int)(saataci * Math.Cos(Math.PI * val / 180));
+            }
+
+            else
+            {
+                coord[0] = OrtaX + (int)(saataci * -Math.Sin(Math.PI * val / 180));
+                coord[1] = OrtaY - (int)(saataci * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Genislik + 1, Yukseklik + 1);
+            OrtaX = Genislik / 2;
+            OrtaY = Yukseklik / 2;
+            this.BackColor = Color.White;
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.timer1_Tick);
+            t.Start();
+        }
+    }
+}
